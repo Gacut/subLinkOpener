646112b912329ffb18a1056aa108d62a15e84c74
Daniel Ancuta <xzibitgacut@gmail.com>
2019-05-10 12:07:43 +0200

    Create subLinkOpener.py

---
+++ subLinkOpener.py
@@ -0,0 +1,52 @@
+#! python3
+
+import webbrowser
+from requests_html import HTMLSession
+
+#skrypt, który otwiera stronę podaną przez użytkownika w postaci'google.pl'.
+def OtwierajStrone(link):
+	webbrowser.get('windows-default').open('https://' + link)
+
+#Skrypt, który otwiera wszystkie podstrony na podanej przez użytkownika stronie.
+def OtwLink(strona):
+	session = HTMLSession()
+	r = session.get('https://' + strona)
+	r.html.absolute_links
+	ls = r.html.absolute_links
+	ls2 = list(ls)
+	ls3 = len(ls2)
+	print('Podstron na podanym adresie: ' + str(ls3))
+	print('Czy otworzyć stronę? t/n')
+	odp = input().lower()
+	if odp == 't':
+		OtwierajStrone(s)
+	elif odp == 'n':
+		print('Ok')
+	else:
+		print('Błąd, włącz program jeszcze raz i podaj poprawną wartość.')
+
+	print('Czy otworzyć podstrony? UWAGA! MOŻE ZAWIESIĆ KOMPUTER!')
+	odp2 = input().lower()
+
+	if odp2 == 't':
+		for i in range (ls3):
+			webbrowser.open(ls2[i])
+	if odp2 == 'n':
+		print('Ok')
+	else:
+		print('Błąd, włącz program jeszcze raz i podaj poprawną wartość.')
+		
+print('Podaj adres strony, która ma być otwarta')
+s = input()
+#pobiera stronę z wartości 's' i uruchamia skrypt
+if s:
+	OtwLink(s)
+
+#Jeżeli użytkownik nie poda żadnej wartości, zostanie poproszony
+#o podanie poprawnej nazwy strony
+else:
+	print('podaj poprawną nazwe strony')
+
+
+#Prosi o podanie adresu przez użytownika
+
